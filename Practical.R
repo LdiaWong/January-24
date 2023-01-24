b196ca9a6cfae49468005a0a3f53d26f72611d42
LdiaWong <lwong014@uottawa.ca>
2023-01-24 16:19:06 -0500

    new branch for script

--- Practical.R
+++ Practical.R
@@ -18,5 +18,13 @@ usethis::use_git()
 usethis::use_github()
 usethis::git_vaccinate()
 
+library(palmerpenguins)
+head(penguins)
+
+#hopefully this shows up in the new branch?
+#let's run a regression:
+summary(lm(body_mass_g ~ flipper_length_mm + species, data = penguins))
+plot(body_mass_g ~ flipper_length_mm, data = penguins)
+
 #To make a new branch
 # https://sizespectrum.org/mizer/articles/working_with_git.html#:~:text=To%20create%20the%20new%20branch,new%20branch%3A%20%E2%80%9Cadd_my_info%E2%80%9D.
